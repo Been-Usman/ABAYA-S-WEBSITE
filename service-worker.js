8682333bdf33556e1e4f8de86de65a9bb4846bcf
Been-Usman <beenusman1530@gmail.com>
2026-09-23 14:58:30 +0300

    Fix: remove Success + splash bigger/centered/no-wrap

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    NAKOWA ABAYAS — Service Worker (v3)
    ============================================================ */
 
-const CACHE_NAME = 'nakowa-v35';
+const CACHE_NAME = 'nakowa-v36';
 const ASSETS = [
     './',
     './index.html',
